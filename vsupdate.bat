0e622f2517667d0fc13316ca92daf7741f741181
Evan Sloan <evansloan082@gmail.com>
2021-02-26 18:57:44 -0500

    check for changed files before copying

--- vsupdate.bat
+++ vsupdate.bat
@@ -5,8 +5,13 @@ cd %~dp0
 echo Checking for world updates...
 git pull origin main
 
-echo Applying world updates...
-xcopy /s "worlds" "%USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds"
+for /f %%i in ('git diff --exit-code worlds') do set updated=%%i
+echo Updated: %updated%
 
-echo Update finished
+if "%updated%" == "" (
+    echo Applying world updates...
+    xcopy /s "worlds" "%USERPROFILE%\AppData\LocalLow\IronGate\Valheim\worlds"
+    echo Update finished
+)
+
 start "valheim.exe" "C:\Program Files (x86)\Steam\steamapps\common\Valheim\valheim.exe"
